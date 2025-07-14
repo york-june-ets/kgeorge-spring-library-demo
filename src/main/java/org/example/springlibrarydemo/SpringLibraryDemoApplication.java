705fd021ec896a9c3e16a79c3ad3f4c3199b23dc
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-14 17:26:35 -0400

    base project

---
+++ src/main/java/org/example/springlibrarydemo/SpringLibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springlibrarydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLibraryDemoApplication.class, args);
+    }
+
+}
